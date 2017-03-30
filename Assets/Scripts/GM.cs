a13ba1be08477d80e166e3378bf7f3afd76c7a9f
Ashley <ashleyseiter8@gmail.com>
2017-03-30 11:04:56 -0400

    beginning of game

---
+++ Assets/Scripts/GM.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GM : MonoBehaviour {
+	public int lives;
+	public int points;
+
+	public void SetLives(int newValue) {
+		lives = newValue;
+
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public int health = 100;
+	public float speed = 5;
+	public float jumpSpeed = 5;
+	public float deadZone = -3;
+
+	new Rigidbody2D rigidbody;
+	GM _GM ;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody = GetComponent<Rigidbody2D> ();
+		_GM = FindObjectOfType<GM> ();
+		
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		//apply movement
+		float x = Input.GetAxisRaw ("Horizontal");
+		Vector2 v = rigidbody.velocity;
+		v.x = x * speed;
+
+		if (Input.GetButtonDown ("Jump")) {
+			v.y = jumpSpeed;
+		}
+		rigidbody.velocity = v;
+
+		//check for out
+		if (transform.position.y < deadZone) {
+			Debug.Log ("You're Out");
+		}
+
+		//rigidbody.AddForce (new Vector2 (x * speed, 0));
+
+	}
+	public void GetOut() {
+		_GM.SetLives (_GM.lives - 1);
+
+	}
+}
