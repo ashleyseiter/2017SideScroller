f0fd7f31cd33bc5bd98cee742baffcb30d13bfe6
Ashley <ashleyseiter8@gmail.com>
2017-04-24 10:49:34 -0400

    Added Throw and Explode to bomb

--- Assets/Scripts/bomb.cs
+++ Assets/Scripts/bomb.cs
@@ -4,16 +4,67 @@ using UnityEngine;
 
 public class bomb : MonoBehaviour {
 
+	public float blastRadius = 5;
+	public bool isActive = false;
+
+	private new Rigidbody2D rigidBody2D;
+	private new Collider2D collider2D;
+
+	void Start() {
+		rigidBody2D = GetComponent <Rigidbody2D> ();
+		collider2D = GetComponent<Collider2D> ();
+	}
+
+	void Update() {
+		if (Input.GetButtonDown("Fire1") && isActive){
+			Throw ();
+		}
+	}
+
 	void OnCollisionEnter2D(Collision2D coll) {
 
 		var player = coll.gameObject.GetComponent<Player> ();
-		if (player != null) { // != not
-			var enemies = FindObjectsOfType<Enemy>();
-			foreach (var e in enemies) {
-				if (Vector3.Distance (this.transform.position, e.transform.position) < 10) {
+		if (player != null && !isActive) { // != not
+			GetPickedUp (player);
+		}
+		if (isActive && player == null) {
+			Explode ();
+		}
+	}
+
+	public void Throw () {
+		
+		collider2D.enabled = true;
+		rigidBody2D.isKinematic = false;
+		rigidBody2D.velocity = new Vector2(5,0);
+		transform.parent = null;
+
+	}
+
+
+	public void GetPickedUp(Player player) {
+		isActive = true;
+		collider2D.enabled = false;
+		rigidBody2D.isKinematic = true;
+		rigidBody2D.velocity = new Vector2 ();
+		transform.parent = player.transform;
+		transform.localScale = new Vector3 (.05f, .05f);
+		transform.localPosition = new Vector3 (.2f, .2f);
+
+
+	}
+
+
+	public void Explode() {
+		
+		var enemies = FindObjectsOfType<Enemy>();
+		foreach (var e in enemies) {
+				if (Vector3.Distance (this.transform.position, e.transform.position) < blastRadius) {
 					e.gameObject.SetActive (false);
 				}
 			}
+			gameObject.SetActive (false);
 		}
 	}
-}
+
+
