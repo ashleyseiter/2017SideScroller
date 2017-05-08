841660bdc0aa681f29fb80c0db7ca3cf4f16fda9
Ashley <ashleyseiter8@gmail.com>
2017-05-08 17:52:43 -0400

    Added Level 2 w new enemy and moving platform

--- Assets/Scripts/Bazooka.cs
+++ Assets/Scripts/Bazooka.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Bazooka : weapon {
 
 	public GameObject rocketPrefab;
+	public float blastRadius = 1;
 
 	public override void Attack ()
 	{
@@ -12,6 +13,23 @@ public class Bazooka : weapon {
 		rocket.transform.position = this.transform.position;
 		rocket.GetComponent<Rigidbody2D> ().velocity = new Vector2 (10, 0);
 		base.Attack ();
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+
+		var player = coll.gameObject.GetComponent<Player> ();
+		if (player != null) {
+			Shoot ();
+		}
+	}
+		public void Shoot() {
+			var enemies = FindObjectsOfType<Enemy>();
+			foreach (var e in enemies) {
+			if (Vector3.Distance (this.transform.position, e.transform.position) < blastRadius) {
+				e.gameObject.SetActive (false);
+			}
+		}
+			gameObject.SetActive (false);
 	}
 
 }
